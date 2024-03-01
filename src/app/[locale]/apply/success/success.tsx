0a83d5f17722b025e13a3ec1d5bdfe5cca92cfaa
tntons <saharat.nuamchit@gmail.com>
2024-03-01 12:06:05 +0900

    chore: apply minor change

--- src/app/[locale]/apply/success/success.tsx
+++ src/app/[locale]/apply/success/success.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { useRouter } from '@/navigation';
 
 export default function Success() {
     const router = useRouter();
